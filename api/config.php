dd683743250979db027eae4998188ca03d344f51
Matt Collins <roramedia@Matthews-MacBook-Pro.local>
2015-05-28 13:30:31 +0100

    Site

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'byb6RM57ErnDg1vzp0dF1jio7');
+    define('CONSUMER_SECRET', 'UALbqhneHdPQMhEBMmMgD5LvysrL1xDR5Vj6MekwbJ9vlFSZnS');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '2735035292-oYp8rlSp42wsmQF7BMQKTllIQ4k7O5nfw7DWBPl');
+    define('ACCESS_SECRET', 'MnmZWxrgfsGlolhkD1nm38Cpyo7LggdpuMXFrqb2jEbvn');
